63206364137c31fb2b567fc5c600e89058738ad3
Dominik Graetz <dominik@dyn-184-171-77-175.uoregon.edu>
2026-01-29 13:28:12 -0800

    initial analysis

--- dominik/analysis_Alternate_Cues.R
+++ dominik/analysis_Alternate_Cues.R
@@ -12,42 +12,56 @@ normWS <- function(d ,s, dv, between = NULL) {
 }
 
 #Alternate Cues
-alt_cue <- import("../data/AlternateCues.csv")
+alt_cue <- import("data/AlternateCues.csv")
 
 exclude_subjects <- alt_cue %>%
   group_by(id) %>% 
   summarize(acc = mean(1-error),
             N = n())
 
+exclude_subjects <- exclude_subjects %>%
+  filter(acc < 0.8)
+
+exclude_subjects <- c(exclude_subjects$id, 201, 901, 902, 959, 975, 4)
+
 alt_cue <- alt_cue %>%
+  filter(!id %in% exclude_subjects) %>%
   group_by(id, block) %>%
   mutate(trialgroup = ifelse(cycle == 1, 1, 0),
          trialgroup = cumsum(trialgroup),
-         is_neutral = ifelse(dim1 == 4 | dim2 == 4, 1, 0),
-         was_prev_neutral = lag(is_neutral, 1),
+         is_bivalent = ifelse(dim1 == 4 | dim2 == 4, 0, 1),
+         was_prev_bivalent = lag(is_bivalent, 1),
          was_prev_error = lag(error, 1),
          prev_task = lag(task, 1),
          switch = ifelse(prev_task == task, 0, 1)) %>%
   group_by(id, block, trialgroup) %>%
-  mutate(was_prev_neutral = was_prev_neutral[1],
-         switch = switch[1]) %>%
+  mutate(was_prev_bivalent = was_prev_bivalent[1]) %>%
   ungroup() %>%
   mutate(id = factor(id),
-         was_prev_neutral = factor(was_prev_neutral),
-         is_neutral = factor(is_neutral),
+         was_prev_bivalent = factor(was_prev_bivalent),
+         is_bivalent = factor(is_bivalent),
          cycle = factor(cycle),
-         switch = factor(switch)) %>%
+         switch = factor(switch),
+         was_prev_bivalent_contr = case_when(was_prev_bivalent == 1 ~ 0.5,
+                                              was_prev_bivalent == 0 ~ -0.5),
+         is_bivalent_contr = case_when(is_bivalent == 1 ~ 0.5,
+                                        is_bivalent == 0 ~ -0.5),
+         switch_contr = case_when(switch == 1 ~ 0,
+                                  switch == 0 ~ 1)) %>%
   filter(trialgroup != 1, block != 0) %>%
-  group_by(id, was_prev_neutral, is_neutral, cycle) %>%
-  mutate(RT_z = (time - mean(time[error == 0 & was_prev_error == 0]))/ sd(time[error == 0 & was_prev_error == 0]),
-         RT_outlier = ifelse(abs(RT_z) > 3, 1, 0))
+  group_by(id, was_prev_bivalent, is_bivalent, cycle) %>%
+  mutate(time_z = scale(time - mean(time[error == 0 & was_prev_error == 0])) %>% as.vector(),
+         time_outlier = ifelse(abs(time_z) > 3, 1, 0),
+         filter_correct = ifelse(error == 1 & was_prev_error == 1, 1, 0),
+         time_log = log(time)) %>%
+  filter(time_outlier == 0)
 
 alt_cue_agg <- alt_cue %>%
-  group_by(id, is_neutral, was_prev_neutral, switch, cycle) %>%
-  summarize(RT = mean(time, na.rm = TRUE)) %>%
+  group_by(id, is_bivalent, was_prev_bivalent, switch, cycle) %>%
+  summarize(RT = mean(time[filter_correct == 0], na.rm = TRUE)) %>%
   normWS(s = "id", dv = "RT") %>%
   mutate(RT = DV_n) %>%
-  group_by(is_neutral, was_prev_neutral, switch, cycle) %>%
+  group_by(is_bivalent, was_prev_bivalent, switch, cycle) %>%
   summarize(RT_m = mean(RT, na.rm = TRUE),
             RT_sd = sd(RT, na.rm = TRUE),
             N = n(),
@@ -56,24 +70,17 @@ alt_cue_agg <- alt_cue %>%
 
 pos <- position_dodge(width = 0.1)
 
-ggplot(alt_cue_agg, aes(x = cycle, y = RT_m, color = is_neutral, linetype = was_prev_neutral, group = interaction(is_neutral, was_prev_neutral)))+
+ggplot(alt_cue_agg, aes(x = cycle, y = RT_m, color = is_bivalent, linetype = was_prev_bivalent, group = interaction(is_bivalent, was_prev_bivalent)))+
   geom_line(position = pos)+
   geom_errorbar(aes(ymin = RT_m-CI, ymax = RT_m+CI), width = 0.1, position = pos)+
   theme_classic()
 
 
-lmer(time ~ was_prev_neutral*is_neutral+(was_prev_neutral*is_neutral|id), data = alt_cue %>% filter(cycle == 1)) %>% summary()
-lmer(time ~ was_prev_neutral*is_neutral+(was_prev_neutral*is_neutral|id), data = alt_cue %>% filter(cycle == 2)) %>% summary()
-lmer(time ~ was_prev_neutral*is_neutral+(was_prev_neutral*is_neutral|id), data = alt_cue %>% filter(cycle == 3)) %>% summary()
 
-lmer(time ~ was_prev_neutral*is_neutral*cycle+(was_prev_neutral*is_neutral*cycle|id), data = alt_cue) %>% summary()
+m1 <- lmer(time ~ is_bivalent_contr * was_prev_bivalent_contr * switch_contr + (is_bivalent_contr + was_prev_bivalent_contr + switch_contr||id), 
+     alt_cue %>% 
+       filter(filter_correct == 0))
 
-intercept <- lmer(time ~ was_prev_neutral*is_neutral+(1|id), data = alt_cue %>% filter(cycle == 1))
-add <- lmer(time ~ was_prev_neutral*is_neutral+(was_prev_neutral+is_neutral|id), data = alt_cue %>% filter(cycle == 1))
-multi <- lmer(time ~ was_prev_neutral*is_neutral+(was_prev_neutral*is_neutral|id), data = alt_cue %>% filter(cycle == 1))
-
-anova(intercept, add, multi)
-
-
+summary(m1)
 
 
--- dominik/analysis_Alternate_Runs.R
+++ dominik/analysis_Alternate_Runs.R
@@ -12,58 +12,45 @@ normWS <- function(d ,s, dv, between = NULL) {
 }
 
 #Alternate Cues
-alt_run <- import("../data/AlternateRuns.csv")
+alt_run <- import("data/AlternateRuns.csv")
 
-alt_run %>% group_by(id, block) %>% summarize(dur = sum(time)/1000,
-                                              max = n())
-
-#this is code with trial sub groups, keeping the cycle variable from 1-8
-# alt_run2 <- alt_run %>%
-#   group_by(id, block) %>%
-#   mutate(trialgroup = NA,
-#          trialgroup = ifelse(cycle == 1, 1, 0),
-#          trialgroup = cumsum(trialgroup),
-#          trialgroup_sub = NA,
-#          trialgroup_sub = ifelse(cycle == 1, 1, NA), #first half
-#          trialgroup_sub = ifelse(cycle == 5, 2, trialgroup_sub), #second half
-#          is_neutral = ifelse(dim1 == 4 | dim2 == 4, 1, 0),
-#          was_prev_neutral = lag(is_neutral, 1),
-#          was_prev_error = lag(error, 1),
-#          prev_task = lag(task, 1),
-#          switch = ifelse(cycle %in% c(1, 5), 1, 0)) %>%
-#   fill(trialgroup_sub) %>%
-#   group_by(id, block, trialgroup, trialgroup_sub) %>%
-#   mutate(was_prev_neutral = was_prev_neutral[1]) %>%
-#   mutate(id = factor(id),
-#          was_prev_neutral = factor(was_prev_neutral),
-#          is_neutral = factor(is_neutral),
-#          cycle = factor(cycle),
-#          switch = factor(switch)) %>%
-#   filter(trialgroup != 1, block != 0, error == 0, was_prev_error == 0)
 
 alt_run <- alt_run %>%
   group_by(id, block) %>%
   mutate(cycle = ifelse(cycle > 4, cycle - 4, cycle), #transforming cycle variable to the range of 1-4
          trialgroup = ifelse(cycle == 1, 1, 0),
          trialgroup = cumsum(trialgroup),
-         is_neutral = ifelse(dim1 == 4 | dim2 == 4, 1, 0),
-         was_prev_neutral = lag(is_neutral, 1),
+         is_bivalent = ifelse(dim1 == 4 | dim2 == 4, 0, 1),
+         was_prev_bivalent = lag(is_bivalent, 1),
          was_prev_error = lag(error, 1),
-         prev_task = lag(task, 1)) %>%
+         prev_task = lag(task, 1),
+         switch = ifelse(prev_task == task, 0, 1)) %>%
   group_by(id, block, trialgroup) %>%
-  mutate(was_prev_neutral = was_prev_neutral[1]) %>%
+  mutate(was_prev_bivalent = was_prev_bivalent[1]) %>%
   mutate(id = factor(id),
-         was_prev_neutral = factor(was_prev_neutral, levels = c("0", "1")),
-         is_neutral = factor(is_neutral, levels = c("0", "1")),
-         cycle = factor(cycle)) %>%
-  filter(trialgroup != 1, block != 0, error == 0, was_prev_error == 0)
+         was_prev_bivalent = factor(was_prev_bivalent),
+         is_bivalent = factor(is_bivalent),
+         cycle = factor(cycle),
+         switch = factor(switch),
+         was_prev_bivalent_contr = case_when(was_prev_bivalent == 1 ~ 0.5,
+                                             was_prev_bivalent == 0 ~ -0.5),
+         is_bivalent_contr = case_when(is_bivalent == 1 ~ 0.5,
+                                       is_bivalent == 0 ~ -0.5),
+         switch_contr = switch) %>%
+  filter(trialgroup != 1, block != 0) %>%
+  group_by(id, was_prev_bivalent, is_bivalent, cycle) %>%
+  mutate(time_z = scale(time - mean(time[error == 0 & was_prev_error == 0])) %>% as.vector(),
+         time_outlier = ifelse(abs(time_z) > 3, 1, 0),
+         filter_correct = ifelse(error == 1 & was_prev_error == 1, 1, 0),
+         time_log = log(time)) %>%
+  filter(time_outlier == 0)
 
 alt_run_agg <- alt_run %>%
-  group_by(id, is_neutral, was_prev_neutral, cycle) %>%
-  summarize(RT = mean(time, na.rm = TRUE)) %>%
+  group_by(id, is_bivalent, was_prev_bivalent, cycle) %>%
+  summarize(RT = mean(time[filter_correct == 0], na.rm = TRUE)) %>%
   normWS(s = "id", dv = "RT") %>%
   mutate(RT = DV_n) %>%
-  group_by(is_neutral, was_prev_neutral, cycle) %>%
+  group_by(is_bivalent, was_prev_bivalent, cycle) %>%
   summarize(RT_m = mean(RT, na.rm = TRUE),
             RT_sd = sd(RT, na.rm = TRUE),
             N = n(),
@@ -73,7 +60,7 @@ alt_run_agg <- alt_run %>%
 
 pos <- position_dodge(width = 0.1)
 
-ggplot(alt_run_agg, aes(x = cycle, y = RT_m, color = is_neutral, linetype = was_prev_neutral, group = interaction(is_neutral, was_prev_neutral)))+
+ggplot(alt_run_agg, aes(x = cycle, y = RT_m, color = is_bivalent, linetype = was_prev_bivalent, group = interaction(is_bivalent, was_prev_bivalent)))+
   geom_line(position = pos)+
   geom_errorbar(aes(ymin = RT_m-CI, ymax = RT_m+CI), width = 0.1, position = pos)+
   theme_classic()
