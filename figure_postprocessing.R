c7763af262881eee4d9e7d1d6722650948063036
Dominik Graetz <dominik@dyn-10-108-15-37.wless.uoregon.edu>
2026-05-12 10:36:10 -0700

    figure_postproc

--- figure_postprocessing.R
+++ figure_postprocessing.R
@@ -2,7 +2,7 @@ figures <- list.files("figures/", pattern = "RDS|rds", full.names = TRUE)
 
 source("theme.R")
 
-# Reading in data ----
+# Reading in figures ----
 
 for (f in figures) {
   obj_name <- gsub(".RDS|.rds", "", basename(f))
@@ -11,24 +11,27 @@ for (f in figures) {
 
 
 
+
+
+
 # Manipulating plots ----
 
 
-ggsave(AC_ddm_a, "figures/postprocessing/AC_ddm_a.svg", width = 5, height = 5, units = "in")
-ggsave(AR_ddm_a, "figures/postprocessing/AR_ddm_a.svg", width = 5, height = 5, units = "in")
+ggsave(plot = AC_ddm_a, "figures/postprocessing/AC_ddm_a.svg", width = 5, height = 5, units = "in")
+ggsave(plot = AR_ddm_a, "figures/postprocessing/AR_ddm_a.svg", width = 5, height = 5, units = "in")
 
-ggsave(AC_ddm_TER, "figures/postprocessing/AC_ddm_TER.svg", width = 5, height = 5, units = "in")
-ggsave(AR_ddm_TER, "figures/postprocessing/AR_ddm_TER.svg", width = 5, height = 5, units = "in")
+ggsave(plot = AC_ddm_TER, "figures/postprocessing/AC_ddm_TER.svg", width = 5, height = 5, units = "in")
+ggsave(plot = AR_ddm_TER, "figures/postprocessing/AR_ddm_TER.svg", width = 5, height = 5, units = "in")
 
-ggsave(AC_ddm_v, "figures/postprocessing/AC_ddm_v.svg", width = 5, height = 5, units = "in")
-ggsave(AR_ddm_v, "figures/postprocessing/AR_ddm_v.svg", width = 5, height = 5, units = "in")
+ggsave(plot = AC_ddm_v, "figures/postprocessing/AC_ddm_v.svg", width = 5, height = 5, units = "in")
+ggsave(plot = AR_ddm_v, "figures/postprocessing/AR_ddm_v.svg", width = 5, height = 5, units = "in")
 
-ggsave(AC_Errors, "figures/postprocessing/AC_Errors.svg", width = 5, height = 5, units = "in")
-ggsave(AR_Errors, "figures/postprocessing/AR_Errors.svg", width = 5, height = 5, units = "in")
+ggsave(plot = AC_Errors, "figures/postprocessing/AC_Errors.svg", width = 5, height = 5, units = "in")
+ggsave(plot = AR_Errors, "figures/postprocessing/AR_Errors.svg", width = 5, height = 5, units = "in")
 
-ggsave(AC_lagRT, "figures/postprocessing/AC_lagRT.svg", width = 5, height = 5, units = "in")
-ggsave(AR_lagRT, "figures/postprocessing/AR_lagRT.svg", width = 5, height = 5, units = "in")
+ggsave(plot = AC_lagRT, "figures/postprocessing/AC_lagRT.svg", width = 5, height = 5, units = "in")
+ggsave(plot = AR_lagRT, "figures/postprocessing/AR_lagRT.svg", width = 5, height = 5, units = "in")
 
 
-ggsave(AC_RTlog, "figures/postprocessing/AC_RTlog.svg", width = 5, height = 5, units = "in")
-ggsave(AR_RTlog, "figures/postprocessing/AR_RTlog.svg", width = 5, height = 5, units = "in")
+ggsave(plot = AC_RTlog, "figures/postprocessing/AC_RTlog.svg", width = 5, height = 5, units = "in")
+ggsave(plot = AR_RTlog, "figures/postprocessing/AR_RTlog.svg", width = 5, height = 5, units = "in")
