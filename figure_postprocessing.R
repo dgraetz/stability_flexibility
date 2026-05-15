61758dcf039793417a6e09929cc4055dabca9e66
Dominik Graetz <dominik@dyn-184-171-77-175.uoregon.edu>
2026-05-15 12:53:14 -0700

    figure updates

--- figure_postprocessing.R
+++ figure_postprocessing.R
@@ -1,6 +1,8 @@
+library(tidyverse)
+source("theme.R")
+
 figures <- list.files("figures/", pattern = "RDS|rds", full.names = TRUE)
 
-source("theme.R")
 
 # Reading in figures ----
 
@@ -13,7 +15,7 @@ for (f in figures) {
 
 get_range <- function(plt_a, plt_b, dv_a, dv_b, eb_a_lower = NULL, eb_a_upper = NULL, eb_b_lower = NULL, eb_b_upper = NULL){
   a_range <- range(plt_a@data[,dv_a])
-  b_range <- range(plt_a@data[,dv_b])
+  b_range <- range(plt_b@data[,dv_b])
   
   dv_range <- c(min(a_range[1], b_range[1]), max(a_range[2], b_range[2]))
   
@@ -233,7 +235,7 @@ ggsave(plot = AR_ddm_v, "figures/postprocessing/AR_ddm_v.svg", width = 3, height
 
 ## Errors ----
 
-
+Error_range <- get_range(AC_Errors, AR_Errors, dv_a = "Error_m", dv_b = "Error_m", eb_a_lower = "Error_ci_lower", eb_b_lower = "Error_ci_lower", eb_a_upper = "Error_ci_upper", eb_b_upper = "Error_ci_upper")
 
 AC_Errors@layers$geom_errorbar$aes_params$linetype = "solid"
 
@@ -245,6 +247,7 @@ AC_Errors <- AC_Errors +
     },
     labels = function(x) sprintf("%.2f", x)
   ) + 
+  coord_cartesian(ylim = Error_range$error)+
   guides(
     linetype = guide_legend(order = 1),  # appears first
     color    = guide_legend(order = 2)   # appears second
@@ -277,6 +280,7 @@ AR_Errors <- AR_Errors +
     },
     labels = function(x) sprintf("%.2f", x)
   ) + 
+  coord_cartesian(ylim = Error_range$error)+
   guides(
     linetype = guide_legend(order = 1),  # appears first
     color    = guide_legend(order = 2)   # appears second
@@ -290,6 +294,8 @@ ggsave(plot = AC_Errors, "figures/postprocessing/AC_Errors.svg", width = 3, heig
 ggsave(plot = AR_Errors, "figures/postprocessing/AR_Errors.svg", width = 3, height = 3, units = "in")
 
 ## log(RT) ----
+
+logRT_range <- get_range(AC_RTlog, AR_RTlog, dv_a = "RT_m", dv_b = "RT_log_m", eb_a_lower = "RT_ci_lower", eb_b_lower = "RT_log_ci_lower", eb_a_upper = "RT_ci_upper", eb_b_upper = "RT_log_ci_upper")
 
 AC_RTlog@layers$geom_errorbar$aes_params$linetype = "solid"
 
@@ -305,6 +311,7 @@ AC_RTlog <- AC_RTlog +
     },
     labels = function(x) sprintf("%.1f", x)
   ) + 
+  coord_cartesian(ylim = logRT_range$error)+
   labs(y = "log(RT)") +
   scale_color_manual(name = "Current Valence",
                      labels = c("Univalent" = "Univalent",
@@ -336,6 +343,7 @@ AR_RTlog <- AR_RTlog +
     },
     labels = function(x) sprintf("%.1f", x)
   ) + 
+  coord_cartesian(ylim = logRT_range$error)+
   labs(y = "log(RT)") +
   
   theme(axis.title.x = element_blank(),
@@ -347,6 +355,8 @@ ggsave(plot = AR_RTlog, "figures/postprocessing/AR_RTlog.svg", width = 3, height
 
 ## lag RT ----
 
+lagRT_range <- get_range(AC_lagRT, AR_lagRT, dv_a = "emmean", dv_b = "emmean", eb_a_lower = "asymp.LCL", eb_b_lower = "asymp.LCL", eb_a_upper = "asymp.UCL", eb_b_upper = "asymp.UCL")
+
 AC_lagRT@layers$geom_errorbar$aes_params$linetype = "solid"
 
 AC_lagRT <- AC_lagRT +
@@ -356,6 +366,7 @@ AC_lagRT <- AC_lagRT +
   )+
   facet_wrap(~lag1_time_log_res_c, labeller = labeller(lag1_time_log_res_c = c("-1.5" = "Lag RT Fast",
                                                                                "1.5" = "Lag RT Slow")))+
+  coord_cartesian(ylim = lagRT_range$error)+
   scale_y_continuous(
     breaks = function(x) {
       b <- seq(-5, max(x), by = 0.2)
@@ -389,6 +400,7 @@ AR_lagRT <- AR_lagRT +
   )+
   facet_wrap(~lag1_time_log_res_w_s_int_c, labeller = labeller(lag1_time_log_res_w_s_int_c = c("-1.5" = "Lag RT Fast",
                                                                                "1.5" = "Lag RT Slow")))+
+  coord_cartesian(ylim = lagRT_range$error)+
   scale_y_continuous(
     breaks = function(x) {
       b <- seq(-5, max(x), by = 0.2)
@@ -425,13 +437,8 @@ AC_betw_RT@layers$geom_point$aes_params$size <- betw_asterisk_size
 AR_betw_RT@layers$geom_point$aes_params$shape <- betw_asterisk_symbol
 AC_betw_RT@layers$geom_point$aes_params$shape <- betw_asterisk_symbol
 
-AR_betw_RT_range <- AR_betw_Error@data$estimate %>% range()
-AC_betw_RT_range <- AC_betw_Error@data$estimate %>% range()
 
-betw_RT_range <- c(min(c(AR_betw_RT_range[1], AC_betw_RT_range[1])), max(c(AR_betw_RT_range[2], AC_betw_RT_range[2])))
-betw_RT_range[1] <- -0.1
-betw_RT_range[2] <- betw_RT_range[2] + 0
-
+betw_RT_range <- c(-0.1, 0.8)
 
 
 AC_betw_RT <- AC_betw_RT + 
@@ -442,7 +449,7 @@ AC_betw_RT <- AC_betw_RT +
     },
     labels = function(x) sprintf("%.1f", x)
   ) + 
-  coord_cartesian(ylim = c(0, betw_RT_range[2]), xlim = c(1, 4.5))+ 
+  coord_cartesian(ylim = betw_RT_range, xlim = c(1, 4.5))+ 
   theme(
     strip.text = element_text(color = "white"),
     strip.background = element_blank(),
@@ -464,7 +471,7 @@ AR_betw_RT <- AR_betw_RT +
     },
     labels = function(x) sprintf("%.1f", x)
   ) + 
-  coord_cartesian(ylim = c(0, betw_RT_range[2]), xlim = c(1, 4.5)) + 
+  coord_cartesian(ylim = betw_RT_range, xlim = c(1, 4.5)) + 
   theme(
     strip.text = element_text(color = "white"),
     strip.background = element_blank(),
@@ -499,12 +506,7 @@ AC_betw_Error@layers$geom_point$aes_params$size <- betw_asterisk_size
 AR_betw_Error@layers$geom_point$aes_params$shape <- betw_asterisk_symbol
 AC_betw_Error@layers$geom_point$aes_params$shape <- betw_asterisk_symbol
 
-AR_betw_Error_range <- AR_betw_Error@data$estimate %>% range()
-AC_betw_Error_range <- AC_betw_Error@data$estimate %>% range()
-
-betw_Error_range <- c(min(c(AR_betw_Error_range[1], AC_betw_Error_range[1])), max(c(AR_betw_Error_range[2], AC_betw_Error_range[2])))
-betw_Error_range[1] <- -0.2
-betw_Error_range[2] <- betw_Error_range[2] + 0.2
+betw_Error_range <- c(-0.1, 0.8)
 
 AC_betw_Error <- AC_betw_Error + 
   scale_y_continuous(
@@ -514,7 +516,7 @@ AC_betw_Error <- AC_betw_Error +
     },
     labels = function(x) sprintf("%.1f", x)
   ) + 
-  coord_cartesian(ylim = c(-0.2, betw_Error_range[2]), xlim = c(1, 4.5))+ 
+  coord_cartesian(ylim = betw_Error_range, xlim = c(1, 4.5)) + 
   theme(
     strip.text = element_text(color = "white"),
     strip.background = element_blank(),
@@ -538,7 +540,7 @@ AR_betw_Error <- AR_betw_Error +
     },
     labels = function(x) sprintf("%.1f", x)
   ) + 
-  coord_cartesian(ylim = c(-0.2, betw_Error_range[2]), xlim = c(1, 4.5)) + 
+  coord_cartesian(ylim = betw_Error_range, xlim = c(1, 4.5)) + 
   theme(
     strip.text = element_text(color = "white"),
     strip.background = element_blank(),
@@ -563,28 +565,72 @@ ggsave(plot = AR_betw_Error, "figures/postprocessing/AR_betw_Error.svg",
 
 ### v ----
 
-AC_betw_v <- AC_betw_v + 
-  scale_y_continuous(
-    breaks = function(x) {
-      b <- seq(0, ceiling(max(x) * 5) / 5, by = 0.5)
-      b
-    },
-    labels = function(x) sprintf("%.1f", x)
-  ) 
-
 
 AR_betw_v@layers$geom_label$aes_params$size <- betw_textsize
 AC_betw_v@layers$geom_label$aes_params$size <- betw_textsize
 
+AR_betw_v@layers$geom_point$aes_params$size <- betw_asterisk_size
+AC_betw_v@layers$geom_point$aes_params$size <- betw_asterisk_size
+
+AR_betw_v@layers$geom_point$aes_params$shape <- betw_asterisk_symbol
+AC_betw_v@layers$geom_point$aes_params$shape <- betw_asterisk_symbol
+
+betw_v_range <- c(-0.1, 0.8)
+
+AC_betw_v <- AC_betw_v + 
+  scale_y_continuous(
+    breaks = function(x) {
+      b <- seq(0, ceiling(max(x) * 5) / 5, by = 0.6)
+      b
+    },
+    labels = function(x) sprintf("%.1f", x)
+  ) + 
+  coord_cartesian(ylim = betw_v_range, xlim = c(1, 4.5)) + 
+  theme(
+    strip.text = element_text(color = "white"),
+    strip.background = element_blank(),
+    # axis.line.x = element_blank(),
+    # axis.ticks.x = element_blank(),
+    # axis.text.x = element_blank(),
+    axis.text.x = element_text(size = 10),
+    axis.title.y = element_blank(),
+    axis.line.y = element_blank(),
+    axis.text.y = element_text(color = "white"),
+    axis.ticks.y = element_line(color = "white")
+  )
+
+AC_betw_v
 
 AR_betw_v <- AR_betw_v + 
   scale_y_continuous(
     breaks = function(x) {
-      b <- seq(0, ceiling(max(x) * 5) / 5, by = 0.5)
+      b <- seq(0, ceiling(max(x) * 5) / 5, by = 0.6)
       b
     },
     labels = function(x) sprintf("%.1f", x)
+  ) + 
+  coord_cartesian(ylim = betw_v_range, xlim = c(1, 4.5)) + 
+  theme(
+    strip.text = element_text(color = "white"),
+    strip.background = element_blank(),
+    # axis.line.x = element_blank(),
+    # axis.ticks.x = element_blank(),
+    # axis.text.x = element_blank(),
+    axis.text.y = element_text(size = 10),
+    axis.text.x = element_text(size = 10),
+    axis.title.y = element_blank()
   )
+AR_betw_v
+
+
+ggsave(plot = AC_betw_Error, "figures/postprocessing/AC_betw_Error.svg", 
+       width = betw_output_size[1], 
+       height = betw_output_size[2], units = "in")
+ggsave(plot = AR_betw_Error, "figures/postprocessing/AR_betw_Error.svg", 
+       width = betw_output_size[1], 
+       height = betw_output_size[2], units = "in")
+
+
 
 
 ggsave(plot = AC_betw_v, "figures/postprocessing/AC_betw_v.svg", width = 3.5, height = 4.3, units = "in")
